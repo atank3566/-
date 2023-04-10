a1ecbc6ee2e57243194889f80009c5d40cb0ba55
Park heewoong <90432570+atank3566@users.noreply.github.com>
2023-04-10 16:36:58 +0900

    baekjoon #2745
    8. 일반수학 -> 1. 진법 변환

--- bj.py
+++ bj.py
@@ -1,23 +1,13 @@
-n = int(input())
+Number = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+n,b = input().split()
+n = ''.join(reversed(n))
+b = int(b)
+result = []
 
-line = 0
-end = 0
-while n > end:
-    line += 1
-    end += line
+for i in n:
+    result.append(Number.index(i))
+sum = 0
+for j in range(len(n)-1,-1,-1):
+    sum += (b**j)*result[j]
 
-top = []
-bottom = []
-if line % 2 == 0:
-    for i in range(1,line+1):
-        top.append(i)
-    for i in range(line,0,-1):
-        bottom.append(i)
-else:
-    for i in range(1,line+1):
-        bottom.append(i)
-    for i in range(line,0,-1):
-        top.append(i)
-index =line-(end-n)-1
-print(f"{top[index]}/{bottom[index]}")
-    
+print(sum)
